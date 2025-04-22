c8e718b7202a5a43497e11b7285f0d4771e5ec2a
timiscode <timilehinolabisi15@gmail.com>
2025-04-22 01:35:23 +0100

    latest update

--- Eks/eks.tf
+++ Eks/eks.tf
@@ -9,15 +9,18 @@ module "eks" {
  
     vpc_id = aws_vpc.eks_vpc.id 
 
-    # eks_managed_node_groups = {
-    #     eks_nodes = {
-    #         desired_capacity = 2
-    #         max_capacity = 3
-    #         min_capacity = 1
-    #         instance_type = "t3.medium"
-    #     }
+    eks_managed_node_groups = {
+        eks_nodes = {
+            desired_capacity = 2
+            max_capacity = 3
+            min_capacity = 1
+            instance_type = "t3.medium"
+        }
 
+    }
     tags = {
         Name = "eks-cluster"
     }
+
+
 }
